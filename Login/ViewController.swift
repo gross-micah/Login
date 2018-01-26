77b2d9796fef8cf00182a883b5b6fb5215cce45d
gross-micah <gross.micah@gmail.com>
2018-01-25 16:55:01 -0800

    navigation segues setup, username passed through

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
+	@IBOutlet weak var textField: UITextField!
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		segue.destination.navigationItem.title = textField.text
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
